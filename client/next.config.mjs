146c2a87eba9b25b1082510bd2dafce7b9593ea6
yusufagbyglu <myusufagabeyoglu@gmail.com>
2025-06-19 15:02:56 +0300

    initial commit

---
+++ client/next.config.mjs
@@ -0,0 +1,24 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "pm-s3-images.s3.us-east-2.amazonaws.com",
+  //       port: "",
+  //       pathname: "/**",
+  //     }
+  //   ]
+  // }
+};
+
+export default nextConfig;
---
+++ client/postcss.config.mjs
@@ -0,0 +1,15 @@
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
+
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
+};
+
+export default config;
